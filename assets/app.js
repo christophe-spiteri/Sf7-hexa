f92428bc890eb1568feb710e489c0ea8b268b6a4
christophe <christophe@cls-scripts.com>
2023-11-05 21:22:17 +0100

    Install HTMX

--- assets/app.js
+++ assets/app.js
@@ -8,7 +8,7 @@
 // any CSS you import will output into a single css file (app.css in this case)
 import './styles/app.scss';
 // You can specify which plugins you need
-
 // start the Stimulus application
 import "bootstrap/scss/bootstrap.scss";
 import "bootstrap-icons/font/bootstrap-icons.scss";
+import 'htmx.org';
--- src/Infrastructure/Symfony/Controller/HomeController.php
+++ src/Infrastructure/Symfony/Controller/HomeController.php
@@ -14,20 +14,26 @@ use Symfony\Component\Routing\Annotation\Route;
 class HomeController extends AbstractController
 {
     #[Route('/html', name: 'app_home')]
-    public function home_html(HomeUseCase $homeUseCase,HomePresenterHtml $homePresenter): Response
+    public function home_html(HomeUseCase $homeUseCase, HomePresenterHtml $homePresenter): Response
     {
-        $homeRequestDto=new HomeRequestDto('Nous sommes le %s');
-        $homeResponseDto=$homeUseCase->execute($homeRequestDto);
+        $homeRequestDto = new HomeRequestDto('Nous sommes le %s');
+        $homeResponseDto = $homeUseCase->execute($homeRequestDto);
 
         return new Response($homePresenter->present($homeResponseDto));
     }
 
     #[Route('/json', name: 'app_homeJson')]
-    public function home_json(HomeUseCase $homeUseCase,HomePresenterJson $homePresenter): JsonResponse
+    public function home_json(HomeUseCase $homeUseCase, HomePresenterJson $homePresenter): JsonResponse
     {
-        $homeRequestDto=new HomeRequestDto('Nous sommes le %s');
-        $homeResponseDto=$homeUseCase->execute($homeRequestDto);
+        $homeRequestDto = new HomeRequestDto('Nous sommes le %s');
+        $homeResponseDto = $homeUseCase->execute($homeRequestDto);
 
         return new JsonResponse($homePresenter->present($homeResponseDto));
+    }
+
+    #[Route('/htmx', name: 'app_homeHtmx')]
+    public function home_htmx(HomeUseCase $homeUseCase, HomePresenterJson $homePresenter): Response
+    {
+        return new Response('Htmx OK');
     }
 }
