92d812d653af9184615a7208fde3a2d05b7f3ffd
christophe <christophe@cls-scripts.com>
2023-11-07 19:20:02 +0100

    home

--- src/Domain/UseCase/Home/HomeUseCase.php
+++ src/Domain/UseCase/Home/HomeUseCase.php
@@ -6,6 +6,7 @@ namespace Domain\UseCase\Home;
 use Domain\UseCase\Home\Request\HomeRequestDto;
 use Domain\UseCase\Home\Response\HomeResponseDto;
 use Symfony\Component\Clock\ClockInterface;
+use Symfony\Component\Clock\DatePoint;
 
 class HomeUseCase
 {
@@ -15,7 +16,8 @@ class HomeUseCase
 
     public function execute(HomeRequestDto $homeRequestDto): HomeResponseDto
     {
-        $laDate = $this->clock->now();
+        $laDate = new datePoint(reference: $this->clock->now());
+        // $laDate =$this->clock->now();
         $response = new HomeResponseDto();
         try {
             $response->setMessage(sprintf($homeRequestDto->message, $laDate->format('d/m/Y H:i')));
